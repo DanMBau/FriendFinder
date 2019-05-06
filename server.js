b2bd051541336ac5058faed5037c6f22f49a9b44
DocHolidayDeluxe <dabauer@me.com>
2019-05-06 14:06:30 -0400

    all

--- server.js
+++ server.js
@@ -1,14 +1,6 @@
-// ==============================================================================
-// DEPENDENCIES
-// Series of npm packages that will be used to give our server useful functionality
-// ==============================================================================
 
 var express = require("express");
 
-// ==============================================================================
-// EXPRESS CONFIGURATION
-// This sets up the basic properties for the express server
-// ==============================================================================
 
 // Tells node that we are creating an "express" server
 var app = express();
@@ -26,8 +18,8 @@ app.use(express.json());
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
 
-require("./routing/apiRoutes")(app);
-require("./routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 // =============================================================================
 // LISTENER
