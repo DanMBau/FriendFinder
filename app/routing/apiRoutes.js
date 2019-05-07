0bebf63d0c783cde94830927f54e0be0a7891143
DocHolidayDeluxe <dabauer@me.com>
2019-05-07 18:02:59 -0400

    final

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,17 +2,9 @@
 var friends = require("../data/friends");
 
 
-// ===============================================================================
-// ROUTING
-// ===============================================================================
 
 module.exports = function (app) {
-    // API GET Requests
-    // Below code handles when users "visit" a page.
-    // In each of the below cases when a user visits a link
-    // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-    // ---------------------------------------------------------------------------
-
+   
     app.get("/api/friends", function (req, res) {
         res.json(friends);
     });
@@ -40,17 +32,13 @@ module.exports = function (app) {
 
             console.log(currentFriend.name);
 
-            // We then loop through all the scores of each friend
             for (var j = 0; j < currentFriend.scores.length; j++) {
                 var currentFriendScore = currentFriend.scores[j];
                 var currentUserScore = userScores[j];
 
-                // We calculate the difference between the scores and sum them into the totalDifference
                 totalDifference += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
             }
-            // If the sum of differences is less then the differences of the current "best match"
             if (totalDifference <= bestMatch.friendDifference) {
-                // Reset the bestMatch to be the new friend.
                 bestMatch.name = currentFriend.name;
                 bestMatch.photo = currentFriend.photo;
                 bestMatch.friendDifference = totalDifference;
@@ -59,7 +47,6 @@ module.exports = function (app) {
 
         friends.push(userData);
 
-        // Return a JSON with the user's bestMatch. This will be used by the HTML in the next page
         res.json(bestMatch);
     });
 };
